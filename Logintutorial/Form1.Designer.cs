53cc2cbeac38ec51832f197d525f5156add0c536
casselscott <casselscott.e@outlook.com>
2024-01-28 11:38:38 -0700

    files upload

---
+++ Logintutorial/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace C__Login_tutorial
+{
+    partial class Tutorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tutorial));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.login_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.password_checkbox = new System.Windows.Forms.CheckBox();
+            this.register_button = new System.Windows.Forms.Button();
+            this.register_exit = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.login_button);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(318, 659);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(58, 522);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Already have an Acoount?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // login_button
+            // 
+            this.login_button.BackColor = System.Drawing.Color.MidnightBlue;
+            this.login_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.login_button.Location = new System.Drawing.Point(93, 560);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(138, 54);
+            this.login_button.TabIndex = 2;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(98, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tutorial";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(62, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(339, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Register for an Account";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(343, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Username";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(458, 157);
+            this.username_textbox.Multiline = true;
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(158, 40);
+            this.username_textbox.TabIndex = 3;
+            this.username_textbox.TextChanged += new System.EventHandler(this.username_textbox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(347, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(458, 271);
+            this.password_textbox.Multiline = true;
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(158, 42);
+            this.password_textbox.TabIndex = 5;
+            this.password_textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // password_checkbox
+            // 
+            this.password_checkbox.AutoSize = true;
+            this.password_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_checkbox.Location = new System.Drawing.Point(489, 334);
+            this.password_checkbox.Name = "password_checkbox";
+            this.password_checkbox.Size = new System.Drawing.Size(127, 19);
+            this.password_checkbox.TabIndex = 6;
+            this.password_checkbox.Text = "Show Password";
+            this.password_checkbox.UseVisualStyleBackColor = true;
+            this.password_checkbox.CheckedChanged += new System.EventHandler(this.password_checkbox_CheckedChanged);
+            // 
+            // register_button
+            // 
+            this.register_button.BackColor = System.Drawing.Color.MidnightBlue;
+            this.register_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_button.ForeColor = System.Drawing.Color.Snow;
+            this.register_button.Location = new System.Drawing.Point(436, 425);
+            this.register_button.Name = "register_button";
+            this.register_button.Size = new System.Drawing.Size(149, 58);
+            this.register_button.TabIndex = 7;
+            this.register_button.Text = "Register";
+            this.register_button.UseVisualStyleBackColor = false;
+            this.register_button.Click += new System.EventHandler(this.register_button_Click);
+            // 
+            // register_exit
+            // 
+            this.register_exit.AutoSize = true;
+            this.register_exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.register_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_exit.Location = new System.Drawing.Point(684, 9);
+            this.register_exit.Name = "register_exit";
+            this.register_exit.Size = new System.Drawing.Size(27, 25);
+            this.register_exit.TabIndex = 8;
+            this.register_exit.Text = "X";
+            this.register_exit.Click += new System.EventHandler(this.register_exit_Click);
+            // 
+            // Tutorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 657);
+            this.Controls.Add(this.register_exit);
+            this.Controls.Add(this.register_button);
+            this.Controls.Add(this.password_checkbox);
+            this.Controls.Add(this.password_textbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.username_textbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Tutorial";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox username_textbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.CheckBox password_checkbox;
+        private System.Windows.Forms.Button register_button;
+        private System.Windows.Forms.Label register_exit;
+    }
+}
+
---
+++ Logintutorial/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+
+namespace C__Login_tutorial
+{
+
+
+    public partial class Tutorial : Form
+
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-7IEOM2U\SQLEXPRESS;Initial Catalog=employee;Integrated Security=True;Connect Timeout=30");
+
+        public Tutorial()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            password_textbox.PasswordChar = password_checkbox.Checked ? '0' : '*';
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void username_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void register_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void register_button_Click(object sender, EventArgs e)
+        {
+
+            if (username_textbox.Text == ""
+               || password_textbox.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields"
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+                        // TO CHECK IF THE USER IS EXISTING ALREADY
+                        string selectUsername = "SELECT COUNT(id) FROM users WHERE username = @users";
+
+                        using (SqlCommand checkUser = new SqlCommand(selectUsername, connect))
+                        {
+                            checkUser.Parameters.AddWithValue("@users", username_textbox.Text.Trim());
+                            int count = (int)checkUser.ExecuteScalar();
+
+                            if (count >= 1)
+                            {
+                                MessageBox.Show(username_textbox.Text.Trim() + " is already taken"
+                                   , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                DateTime today = DateTime.Today;
+
+                                string insertData = "INSERT INTO users " +
+                                    "(username, password, date_register) " +
+                                    "VALUES(@username, @password, @dateReg)";
+
+                                using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                                {
+                                    cmd.Parameters.AddWithValue("@username", username_textbox.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@password", password_textbox.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@dateReg", today);
+
+                                    cmd.ExecuteNonQuery();
+
+                                    MessageBox.Show("Registered successfully!"
+                                        , "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
